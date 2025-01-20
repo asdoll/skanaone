54a40008bb985abfdbc07d8c18a5fe32b5cbe38f
asdoll <ludeqqwo@gmail.com>
2025-01-20 22:31:16 +0800

    fix page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          src="/skanaone/skana.svg"
+          src="/skana.svg"
           alt="Skana.one logo"
           width={180}
           height={38}
@@ -31,7 +31,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/skanaone/vercel.svg"
+              src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
